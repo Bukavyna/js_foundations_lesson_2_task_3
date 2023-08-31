8f1f25aad03a02e7ad22d34d745f95e085683503
Taras <luchakt2@gmail.com>
2023-08-31 10:34:49 +0300

    initial

---
+++ js/script.js
@@ -0,0 +1,30 @@
+"use strict"
+
+let numeric = prompt('Введіть число');
+
+	if (numeric == '1')
+			alert ('Січень');
+	else if (numeric == '2')
+			alert ('Лютий');
+	else if (numeric == '3')
+			alert ('Березень');
+	else if (numeric == '4')
+			alert ('Квітень');
+	else if (numeric == '5')
+			alert ('Травень');
+	else if (numeric == '6')
+			alert ('Червень');
+	else if (numeric == '7')
+			alert ('Липень');
+	else if (numeric == '8')
+		alert ('Серпень');
+	else if (numeric == '9')
+		alert ('Вересень');
+	else if (numeric == '10')
+		alert ('Жовтень');
+	else if (numeric == '11')
+		alert ('Листопад');
+	else if (numeric == '12')
+		alert ('Грудень');
+	else if (numeric > '12')
+		alert ('Некоректний ввід!');
